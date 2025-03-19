478e421489e31fe027c6996bc938301f1e753c29
rihan17 <rihanamorim17@gmail.com>
2025-03-19 13:54:52 -0300

    correcao

--- Revisão Pw3/gestao/php/produto.php
+++ Revisão Pw3/gestao/php/produto.php
@@ -4,14 +4,14 @@ include("conexao.php");
 
 $method = $_SERVER['REQUEST_METHOD'];
 $input = json_decode(file_get_contents("php://input"),true);
-$id = $_GET['id_categoria'] ?? null;
+$id = $_GET['id_produto'] ?? null;
 
 switch ($method) {
     //Consulta no Banco
     case 'GET':
-        $sql = 'SELECT * FROM tb_categoria ';
+        $sql = 'SELECT * FROM tb_produto ';
         if($id>0){
-            $sql .= ' WHERE id_categoria = ' . $id;
+            $sql .= ' WHERE id_produto = ' . $id;
         }
         $resultado = $conexao->query($sql);
         $dados = [];
@@ -22,19 +22,28 @@ switch ($method) {
         break;
     case 'POST':
         // Cadastro no banco
-        $sql = 'INSERT INTO tb_categoria VALUE (null, "'.$input[0]['nm_categoria'].'")';
+        $sql = 'INSERT INTO tb_produto (nm_produto, descricao, vl_produto, id_categoria) 
+                VALUES ("'.$input[0]['nm_produto'].'", 
+                        "'.$input[0]['descricao'].'", 
+                        '.$input[0]['vl_produto'].', 
+                        '.$input[0]['id_categoria'].')';
         $resultado = $conexao->query($sql);
         echo json_encode($conexao->insert_id);
         break;
     case 'PUT':
         //Atualizar um Registro
-        $sql = 'UPDATE tb_categoria SET nm_categoria="'.$input[0]['nm_categoria'].'" WHERE id_categoria='.$input[0]['id_categoria'];
+        $sql = 'UPDATE tb_produto SET 
+                nm_produto="'.$input[0]['nm_produto'].'", 
+                descricao="'.$input[0]['descricao'].'", 
+                vl_produto='.$input[0]['vl_produto'].', 
+                id_categoria='.$input[0]['id_categoria'].' 
+                WHERE id_produto='.$input[0]['id_produto'];
         $resultado = $conexao->query($sql);
         echo json_encode(["atualizado" => $conexao->affected_rows]);
         break;
     case 'DELETE':
         //Excluir um Registro
-        $sql = 'DELETE FROM tb_categoria WHERE id_categoria = ' . $id;
+        $sql = 'DELETE FROM tb_produto WHERE id_produto = ' . $id;
         $resultado = $conexao->query($sql);
         echo json_encode(["excluido" => $conexao->affected_rows]);
         break;
