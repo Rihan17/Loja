2e16ac1d4ef3cfaa992acf81b87701b2080c7940
rihan17 <rihanamorim17@gmail.com>
2025-03-19 13:52:32 -0300

    att

--- Revisão Pw3/gestao/php/produto.php
+++ Revisão Pw3/gestao/php/produto.php
@@ -4,19 +4,39 @@ include("conexao.php");
 
 $method = $_SERVER['REQUEST_METHOD'];
 $input = json_decode(file_get_contents("php://input"),true);
+$id = $_GET['id_categoria'] ?? null;
 
-switch($method){
+switch ($method) {
+    //Consulta no Banco
     case 'GET':
-        echo "é get";
+        $sql = 'SELECT * FROM tb_categoria ';
+        if($id>0){
+            $sql .= ' WHERE id_categoria = ' . $id;
+        }
+        $resultado = $conexao->query($sql);
+        $dados = [];
+        while($linha = $resultado->fetch_object()){
+            $dados[] = $linha;
+        }
+        echo json_encode($dados);
         break;
     case 'POST':
-
+        // Cadastro no banco
+        $sql = 'INSERT INTO tb_categoria VALUE (null, "'.$input[0]['nm_categoria'].'")';
+        $resultado = $conexao->query($sql);
+        echo json_encode($conexao->insert_id);
         break;
     case 'PUT':
-        echo "é put";
+        //Atualizar um Registro
+        $sql = 'UPDATE tb_categoria SET nm_categoria="'.$input[0]['nm_categoria'].'" WHERE id_categoria='.$input[0]['id_categoria'];
+        $resultado = $conexao->query($sql);
+        echo json_encode(["atualizado" => $conexao->affected_rows]);
         break;
     case 'DELETE':
-        echo "é delete";
+        //Excluir um Registro
+        $sql = 'DELETE FROM tb_categoria WHERE id_categoria = ' . $id;
+        $resultado = $conexao->query($sql);
+        echo json_encode(["excluido" => $conexao->affected_rows]);
         break;
     default:
         echo "Acesso inválido";
